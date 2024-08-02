8ada0b833c7a9aa2ec57466ccc041d595d1d0178
martin_paura_bersan <mpaurabe@itba.edu.ar>
2024-08-02 12:52:26 -0300

    Sum4B Rev 0

--- sumador4B/Fuente/sum4b.vhd
+++ sumador4B/Fuente/sum4b.vhd
@@ -1,23 +1,48 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity sum1b is
+entity sum4b is
+    --Parte Declarativa
+    --Generic
+    generic(
+    N: natural :=4
+    );
+    --port    
     port(
-        a_i: in std_logic;
-        b_i: in std_logic;
+        a_i: in std_logic_vector (N-1 downto 0);
+        b_i: in std_logic_vector (N-1 downto 0);
         ci_i: in std_logic;
-        s_o: out std_logic;
+        s_o: out std_logic_vector (N-1 downto 0);
         co_o: out std_logic
     );
-end sum1b;
+end sum4b;
 
-architecture sum1b_arch of sum1b is
---Parte Declarativa
---Generic
---port    
---signal 
+architecture sum4b_arch of sum4b is
+    --Parte Declarativa
+    --Generic
+    --port    
+    component sum1b is
+        port(
+            a_i: in std_logic;
+            b_i: in std_logic;
+            ci_i: in std_logic;
+            s_o: out std_logic;
+            co_o: out std_logic
+        );
+        end component;
+    --signal
+    signal aux: std_logic_vector(N downto 0);
 begin
---Parte Descriptiva
-    s_o <= a_i xor b_i xor ci_i;
-    co_o <= (a_i and b_i) or (a_i and ci_i) or (b_i and ci_i);
-end;-- neg_arch ; -- arch
+    --Parte Descriptiva
+    aux(0) <= ci_i;
+    sum_Nbits_inst: for i in 0 to N-1 generate
+        sum_1Bit_inst: sum1b port map(
+            a_i => a_i(i),
+            b_i => b_i(i),
+            ci_i => aux(i),
+            s_o => s_o(i),
+            co_o =>aux(i+1)
+        );
+    end generate;
+    co_o <= aux(N);
+end;-- sum4b_arch ;
--- sumador4B/Fuente/sum4b_tb.vhd
+++ sumador4B/Fuente/sum4b_tb.vhd
@@ -1,36 +1,46 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity sum1b_tb is
+entity sum4b_tb is
 --No necesito ni entradas ni salidas porque las señales las voy a simular
-end sum1b_tb;
+end sum4b_tb;
 
-architecture sum1b_tb_arch of sum1b_tb is
+architecture sum4b_tb_arch of sum4b_tb is
 --Parte Declarativa
-   component sum1b is
+   component sum4b is
+      --Parte Declarativa
+      --Generic
+      generic(
+      N: natural :=4
+      );
+      --port    
       port(
-         a_i: in std_logic_vector [3 downto 0];
-         b_i: in std_logic;
+         a_i: in std_logic_vector (N-1 downto 0);
+         b_i: in std_logic_vector (N-1 downto 0);
          ci_i: in std_logic;
-         s_o: out std_logic;
+         s_o: out std_logic_vector (N-1 downto 0);
          co_o: out std_logic
       );
    end component;
-
-   signal a_i_tb: std_logic := '0';
-   signal b_i_tb: std_logic := '0';
+   
+   --signal
+   signal a_i_tb: std_logic_vector (3 downto 0) := "0000";
+   signal b_i_tb: std_logic_vector (3 downto 0) := "0011";
    signal ci_i_tb: std_logic := '0';
-   signal s_o_tb: std_logic;
+   signal s_o_tb: std_logic_vector (3 downto 0);
    signal co_o_tb: std_logic;
 
 
 begin
--- Parte Descriptiva
-   a_i_tb <= not a_i_tb after 10 ns;
-   b_i_tb <= not b_i_tb after 20 ns;
-   ci_i_tb <= not ci_i_tb after 40 ns;
-    --Device Under Test
-   DUT: sum1b 
+   -- Parte Descriptiva
+   a_i_tb <="0111" after 10 ns;
+   b_i_tb <= "0000" after 20 ns;
+   ci_i_tb <= '1' after 40 ns;
+   --Device Under Test
+   DUT: sum4b 
+      generic map(
+         N=>4
+      )
       port map(
          a_i => a_i_tb,
          b_i => b_i_tb,
@@ -39,4 +49,4 @@ begin
          co_o => co_o_tb
       );
 
-end; --my_neg_tb_arch 
+end; --sum4b_tb_arch 
